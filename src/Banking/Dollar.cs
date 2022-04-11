41d3cdbbff08f805cbac08e2ea60347bcfc47fda
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2022-04-11 08:18:07 +0200

    added identity tests

--- src/Banking/Dollar.cs
+++ src/Banking/Dollar.cs
@@ -1,5 +1,5 @@
 ﻿namespace Banking;
-public class Dollar
+public class Dollar : IEquatable<Dollar>
 {
   public int Value { get; }
 
@@ -12,4 +12,38 @@ public class Dollar
   {
     return new Dollar(Value * amount);
   }
+
+  #region Equality
+
+  public bool Equals(Dollar? other)
+  {
+    if (ReferenceEquals(null, other)) return false;
+    if (ReferenceEquals(this, other)) return true;
+    return Value == other.Value;
+  }
+
+  public override bool Equals(object? obj)
+  {
+    if (ReferenceEquals(null, obj)) return false;
+    if (ReferenceEquals(this, obj)) return true;
+    if (obj.GetType() != this.GetType()) return false;
+    return Equals((Dollar) obj);
+  }
+
+  public override int GetHashCode()
+  {
+    return Value;
+  }
+
+  public static bool operator ==(Dollar? left, Dollar? right)
+  {
+    return Equals(left, right);
+  }
+
+  public static bool operator !=(Dollar? left, Dollar? right)
+  {
+    return !Equals(left, right);
+  }
+
+  #endregion
 }
--- tests/Banking.Tests/DollarTests.cs
+++ tests/Banking.Tests/DollarTests.cs
@@ -15,4 +15,10 @@ public class DollarTests
         five.Value.Should().Be(5);
         ten.Value.Should().Be(10);
     }
+
+    [Fact]
+    public void DollarsIdentityIsItsValue()
+    {
+        (new Dollar(5)).Should().Be(new Dollar(5));
+    }
 }
